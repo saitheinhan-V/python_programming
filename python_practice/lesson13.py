c89cc5b8eb046c00523aff7a10f911030023a0ee
saitheinhan-V <saihlainhan1541997@gmail.com>
2026-03-26 12:20:14 +0630

    function 2632026

--- python_practice/lesson13.py
+++ python_practice/lesson13.py
@@ -2,3 +2,25 @@ def myFunction(name):
     print("Hello",name)
 
 myFunction("Alice")
+
+def my_function(*args):
+  print("Type:", type(args))
+  print("First argument:", args[0])
+  print("Second argument:", args[1])
+  print("All arguments:", args)
+
+my_function("Emil", "Tobias", "Linus")
+
+def my_function(greeting, *names):
+  for name in names:
+    print(greeting, name)
+
+my_function("Hello", "Emil", "Tobias", "Linus")
+
+def mySum(*args):
+    sum = 0
+    for arg in args:
+        sum += arg
+    return sum
+print(mySum(1, 2, 3))
+print(mySum(4, 5, 6, 7))
---
+++ python_practice/lesson14.py
@@ -0,0 +1,17 @@
+def my_function(**myvar):
+  print("Type:", type(myvar))
+  print("Name:", myvar["name"])
+  print("Age:", myvar["age"])
+  print("All data:", myvar)
+
+my_function(name = "Tobias", age = 30, city = "Bergen")
+
+def myfunc1():
+  x = "Jane"
+  def myfunc2():
+    nonlocal x
+    x = "hello"
+  myfunc2()
+  return x
+
+print(myfunc1())
