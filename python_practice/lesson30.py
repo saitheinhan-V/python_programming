ab888c3a1d30fff259cb3899c2c726bccd0a7290
saitheinhan-V <saihlainhan1541997@gmail.com>
2026-04-01 11:51:52 +0630

    FileOS

--- python_practice/lesson30.py
+++ python_practice/lesson30.py
@@ -1,3 +1,4 @@
+import os
 # f = open("demofile.txt", "rt")
 # print(f.read())
 # f.close()
@@ -8,3 +9,17 @@ with open("demofile.txt", "w") as f:
 #open and read the file after the overwriting:
 with open("demofile.txt", "r") as f:
   print(f.read())
+  f.close()
+
+  #x = write / a = append / r = read / w = write (overwrites the file)
+  if os.path.exists("mytext.txt"):
+    # f = open("mytext.txt","rt")
+    # print(f.read())
+    # f.close()
+    os.remove("mytext.txt") #removes the file
+    # os.rmdir("myfolder") #removes the folder
+  else:
+    f = open("mytext.txt","x")
+    f.write("Hello World!")
+    print(f.readline())
+    f.close()
